543e768f06df976c938f5369aff23709aa8e1f82
adamux <abeckovsky@hertz.com>
2019-12-29 17:22:49 +0000

    CF 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 console.log("SUCCESS - scripts.js loaded!!")
+
+// MENU BUTTON ACTIONS //
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+
+// SMOOTH SCROLLING //
+$("a[href^='#']").click(function(e) {
+  e.preventDefault();
+
+  var position = $($(this).attr("href")).offset().top;
+
+  $("body, html").animate({
+    scrollTop: position
+  } ,1000, );
+});
